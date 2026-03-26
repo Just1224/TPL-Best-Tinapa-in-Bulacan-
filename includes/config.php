00b0a2142bd59b6fb4b43a2e41153389c3015b82
Chichan1310 <138295091+happyvirus-kai@users.noreply.github.com>
2026-03-26 18:10:46 +0800

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,13 @@
 <?php
-$host = getenv('DB_HOST') ?: 'localhost';
+$host = getenv('DB_HOST') ?: '127.0.0.1';
 $user = getenv('DB_USER') ?: 'root';
 $pass = getenv('DB_PASS') ?: '';
 $db   = getenv('DB_NAME') ?: 'tinapa_cms';
+$port = getenv('DB_PORT') ?: 3306;
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
-if(!$conn){
+if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
